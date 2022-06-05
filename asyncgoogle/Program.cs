f3e48b3a537acb27862393eec170a404f3bd868d
psi9 <semyon.pavlenko@gmail.com>
2022-06-05 20:32:32 +1000

    Add project files.

---
+++ asyncgoogle/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace asyncgoogle
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("1 " + Thread.CurrentThread.ManagedThreadId);
+            var client = new HttpClient();
+            var task = Privet(client);
+            Console.WriteLine("11 " + Thread.CurrentThread.ManagedThreadId);
+            var a = 0;
+            for (var i = 0; i < 10000000; i++)
+            {
+                a = i++;
+            }
+            Console.WriteLine("2 " + Thread.CurrentThread.ManagedThreadId);
+            var page = await task;
+            Console.WriteLine("3 " + Thread.CurrentThread.ManagedThreadId);
+            Console.WriteLine("hello");
+        }
+
+        public static async Task<string> Privet(HttpClient client)
+        {
+            Console.WriteLine("sub " +Thread.CurrentThread.ManagedThreadId);
+            await Task.Delay(2000);
+            Console.WriteLine("sub after delay" + Thread.CurrentThread.ManagedThreadId);
+            return await client.GetStringAsync("https://google.com");
+        }
+    }
+}
---
+++ asynctea/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace asynctea
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var tea = await MakeTeaAsync();
+            Console.WriteLine(tea);
+        }
+
+        public static async Task<string> MakeTeaAsync()
+        {
+            Console.WriteLine("main thread " + Thread.CurrentThread.ManagedThreadId);
+            var boilingWaterTask = BoilWaterAsync();
+            Console.WriteLine("take the cups out");
+            Console.WriteLine("main thread " + Thread.CurrentThread.ManagedThreadId);
+
+            var a = 0;
+            for (var i = 0; i < 100_000_000; i ++ )
+            {
+                a =+ i;
+            }
+            Console.WriteLine("main thread after inc" + Thread.CurrentThread.ManagedThreadId);
+
+            Console.WriteLine("put tea in cups");
+            var water = await boilingWaterTask;
+            var tea = $"poor {water} in cups";
+            Console.WriteLine("main thread after water" + Thread.CurrentThread.ManagedThreadId);
+            return tea;
+        }
+
+        public static async Task<string> BoilWaterAsync()
+        {
+            Console.WriteLine("start the kettle");
+            Console.WriteLine("waiting for the kettle");
+            Console.WriteLine("boil thread " + Thread.CurrentThread.ManagedThreadId);
+            await Task.Delay(2000);
+            Console.WriteLine("boil thread " + Thread.CurrentThread.ManagedThreadId);
+            Console.WriteLine("kettle finished boiling");
+            return "water";
+        }
+    }
+}
